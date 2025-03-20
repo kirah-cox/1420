1dc669c6137fca0e42606147047f77d68b762096
kirah.cox <kirahanncox@gmail.com>
2025-03-20 16:19:10 -0600

    Add project files.

---
+++ 1420/Warrior.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace BattleArenaNP
+{
+    public class Warrior : Fighter
+    {
+        public Warrior(string name, int health, int attackPower) : base(name, health, attackPower) { }
+
+        public override int Attack()
+        {
+            return AttackPower;
+        }
+    }
+}
---
+++ 1420/Wizard.cs
@@ -0,0 +1,13 @@
+﻿
+namespace BattleArenaNP
+{
+    public class Wizard : Fighter
+    {
+        public Wizard(string name, int health, int attackPower) : base(name, health, attackPower) { }
+
+        public override int Attack()
+        {
+            return AttackPower;
+        }
+    }
+}
