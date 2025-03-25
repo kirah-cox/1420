968c40c9e3cf0d9baec1c92f44c187194913883d
kirah-cox <kirahanncox@gmail.com>
2025-03-24 22:04:31 -0600

    Finished this assignment

--- 1420/Program.cs
+++ 1420/Program.cs
@@ -4,15 +4,7 @@ class Program
 {
     static void Main()
     {
-        SpyToolkit spyToolkit = new SpyToolkit();
-        spyToolkit.AddGadget("Freeze Ray", "Weapon");
-        spyToolkit.AddGadget("Fire Ray", "Weapon");
-        spyToolkit.AddGadget("Freeze Camera", "Surveillance");
-
-
-        spyToolkit.DeactivateGadget("Freeze Ray");
-
-        spyToolkit.GetActiveGadgets();
+        // Cool Stuff
     }
 }
 
